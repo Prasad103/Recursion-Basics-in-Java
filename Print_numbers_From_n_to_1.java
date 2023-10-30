ded4f7a9aa7c5aef0d006127202c9b1034f67057
Prasad Bebale <119681991+Prasad103@users.noreply.github.com>
2023-10-30 16:13:52 +0530

    Update and rename Recursion.java to Print_numbers_From_n_to_1.java

--- Print_numbers_From_n_to_1.java
+++ Print_numbers_From_n_to_1.java
@@ -1,6 +1,6 @@
 //Print numbers from n to 1 (Decreasing Order)
 
-public class Recursion {
+public class Print_numbers_From_n_to_1 {
     public static void printDec(int n) {
             if( n==1 ) {
                 System.out.println(n);
